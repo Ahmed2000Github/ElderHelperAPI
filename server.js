27b35b0241ce644b27c32e1289b14fd35f53a241
Ahmed2000Github <93035933+Ahmed2000Github@users.noreply.github.com>
2022-02-16 19:51:11 +0100

    up

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ try {
 dotenv.config({
   path: "./config/config.env",
 });
-// const PORT = 1235;
-// const server = http.createServer(app);
-// server.listen(process.env.PORT || 5000);
-const server = http.createServer(process.env.PORT || 3000);
+const PORT = process.env.PORT|| 5000;
+const server = http.createServer(app);
+console.log("hello zorld ");
+server.listen(PORT);
+// const server = http.createServer(process.env.PORT || 3000);
